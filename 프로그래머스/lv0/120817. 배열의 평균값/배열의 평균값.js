0be6f87f7e437e40122a00aeed92049d3c695609
eunseonJeong <121423796+eunseonJeong@users.noreply.github.com>
2023-02-17 12:02:09 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,12 @@
+//평균합 구하는 방법: (첫번째 숫자+끝 숫자)/2 + 원래 숫자 곗수//
+// a,b
+
+//number들의 합을 number.length로 나눔//
+function solution(numbers) {
+    var answer = 0;
+    for (let i = 0; i < numbers.length;i++){
+        answer += numbers[i]                 //answer = numbers의 합
+    }
+    return answer/numbers.length
+
+}
